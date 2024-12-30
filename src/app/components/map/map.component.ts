ae76e2ba6e3ad91d9c056f2873b985153dea0622
ramesty <80014225+ramesty@users.noreply.github.com>
2024-12-30 11:16:00 -0700

    What I have done for this sprint: 	- Currently just playing around with google maps api to see if I can get it to work 	- Installed google maps using the following command: npm install @angular/google-maps@18.2.0 --legacy-peer-deps 		○ Had to do this since maps needs angular 19, and we are currently using 18 for some reason. Can probably change later. Would need to uninstall the library. 	- Got it to work, but currently the API key can be seen on the frontend. This is a security concern, and would like to avoid this

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [GoogleMapsModule],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent {
-
+  center = { lat: 37.7749, lng: -122.4194 }; // Example: San Francisco
+  zoom = 12;
 }
