895d860ad716168b66492fbce245793697dca514
ramesty <80014225+ramesty@users.noreply.github.com>
2025-01-02 14:10:55 -0700

    Relatively minor changes. Added the advanced marker functionality, so now you should see two markers displayed on the map, one in San Francisco and the other in LA. The idea here is to make them dynamic, such that when we get real lat/lng data, it should be relatively straightforward to add it in. Pretty much all I did, also added a Map ID key, not sure if it is required but I added it to map. Options.

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 
+
 @Component({
   selector: 'app-map',
   standalone: true,
@@ -9,6 +10,15 @@ import { GoogleMapsModule } from '@angular/google-maps';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
-  center = { lat: 37.7749, lng: -122.4194 }; // Example: San Francisco
-  zoom = 12;
+  
+  options: google.maps.MapOptions = {
+    mapId: "2b000ff322849d05",
+    center: { lat: 37.7749, lng: -122.4194 },
+    zoom: 12,
+  };
+  
+  markers = [
+    { lat: 37.7749, lng: -122.4194 }, // San Francisco
+    { lat: 34.0522, lng: -118.2437 }  // Los Angeles
+  ];
 }
