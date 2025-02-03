c7be99c7a04cca3784c4ab1590d6d234a859c5fc
ramesty <80014225+ramesty@users.noreply.github.com>
2025-02-03 15:10:22 -0700

    changes to obsolete branch

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -13,10 +13,10 @@ export class MapComponent {
   
   options: google.maps.MapOptions = {
     mapId: "2b000ff322849d05",
-    center: { lat: 37.7749, lng: -122.4194 },
+    center: { lat: 37.7749, lng: -122.4194 }, // San Francisco
     zoom: 12,
   };
-  
+
   markers = [
     { lat: 37.7749, lng: -122.4194 }, // San Francisco
     { lat: 34.0522, lng: -118.2437 }  // Los Angeles
